44b0ac7c8339d887556d524f3e34d17f51edc2f2
Miguel Ángel Bermea Rdz <a01411671@itesm.mx>
2022-11-29 01:57:04 -0600

    Added more categories in UserData page, STILL PENDING DB CONNECTION

--- deer-sign/src/Pages/UserData.js
+++ deer-sign/src/Pages/UserData.js
@@ -6,7 +6,7 @@ import DonutUserChart from "../Components/doughnutUserChart";
 import Navbar from "../Components/Navbar/Navbar";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faA, faFutbol, faMugSaucer, faPalette, faPaw, faPersonRunning, faScrewdriverWrench, faUtensils, faZ } from "@fortawesome/free-solid-svg-icons";
+import { fa1, fa2, fa3, faA, faAppleWhole, faBriefcase, faCalendarDays, faCalendarWeek, faCarrot, faCircleQuestion, faClock, faFileLines, faFutbol, faHandshake, faHouseChimneyWindow, faLocationDot, faMugSaucer, faPalette, faPaw, faPeopleGroup, faPerson, faPersonRunning, faScrewdriverWrench, faShirt, faUtensils, faVanShuttle, faVenusMars, faZ } from "@fortawesome/free-solid-svg-icons";
 
 import '../SCSS/Elements/_userdata.scss'
 
@@ -20,7 +20,7 @@ export default class UserData extends Component {
                 <Navbar />
                 <div className="userdata-container">
                     <div className="userfullname-container">
-                        <h1>GRACE HOPPER</h1>
+                        <h1>NOMBRE COMPLETO</h1>
                     </div>
                     <div className="sections-container">
                         <div class='row'>
@@ -133,18 +133,18 @@ export default class UserData extends Component {
                                                 <Typography className="card-title">VERBOS</Typography>
                                             </CardContent>
                                         </Card>
+                                        <Card id="verbs-card-two" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faPersonRunning} className="card-icon"/>
+                                                <Typography className="card-title">VERBOS</Typography>
+                                            </CardContent>
+                                        </Card>
                                         <Card id="drinks-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
                                             <CardContent>
                                                 <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
                                                 <FontAwesomeIcon icon={faMugSaucer} className="card-icon"/>
                                                 <Typography className="card-title">BEBIDAS</Typography>
-                                            </CardContent>
-                                        </Card>
-                                        <Card id="tools-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
-                                            <CardContent>
-                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
-                                                <FontAwesomeIcon icon={faScrewdriverWrench} className="card-icon"/>
-                                                <Typography className="card-title">HERRAMIENTAS</Typography>
                                             </CardContent>
                                         </Card>
                                         <Card id="animals-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
@@ -153,8 +153,140 @@ export default class UserData extends Component {
                                                 <FontAwesomeIcon icon={faPaw} className="card-icon"/>
                                                 <Typography className="card-title">ANIMALES</Typography>
                                             </CardContent>
+                                        </Card>                                        
+                                    </div>
+                                    <div className="categories-container-first">
+                                        <Card id="body-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px"}} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faPerson} className="card-icon"/>
+                                                <Typography className="card-title">CUERPO</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="days-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faCalendarWeek} className="card-icon"/>
+                                                <Typography className="card-title">DIAS</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="fruits-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faAppleWhole} className="card-icon"/>
+                                                <Typography className="card-title">FRUTAS</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="vegies-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faCarrot} className="card-icon"/>
+                                                <Typography className="card-title">VERDURAS</Typography>
+                                            </CardContent>
                                         </Card>
                                     </div>
+                                    <div className="categories-container-second">
+                                        <Card id="places-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faLocationDot} className="card-icon"/>
+                                                <Typography className="card-title">LUGARES</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="months-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faCalendarDays} className="card-icon"/>
+                                                <Typography className="card-title">MESES</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="num-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={fa1} className="card-icon num"/>
+                                                <FontAwesomeIcon icon={fa2} className="card-icon num"/>
+                                                <FontAwesomeIcon icon={fa3} className="card-icon num"/>
+                                                <Typography className="card-title">NUMEROS</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="house-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faHouseChimneyWindow} className="card-icon"/>
+                                                <Typography className="card-title">HOGAR</Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                    <div className="categories-container-first">
+                                        <Card id="people-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px"}} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faPeopleGroup} className="card-icon"/>
+                                                <Typography className="card-title">PERSONAS</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="questions-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faCircleQuestion} className="card-icon"/>
+                                                <Typography className="card-title">PREGUNTAS</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="prep-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faFileLines} className="card-icon"/>
+                                                <Typography className="card-title">PREPOSICIONES</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="pron-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faVenusMars} className="card-icon"/>
+                                                <Typography className="card-title">PRONOMBRES</Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                    <div className="categories-container-second">
+                                        <Card id="work-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faBriefcase} className="card-icon"/>
+                                                <Typography className="card-title">OFICIOS</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="clothes-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faShirt} className="card-icon"/>
+                                                <Typography className="card-title">ROPA</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="time-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faClock} className="card-icon num"/>
+                                                <Typography className="card-title">TIEMPO</Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card id="transport-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faVanShuttle} className="card-icon"/>
+                                                <Typography className="card-title">TRANSPORTE</Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                    <div className="categories-container-first">
+                                        <Card id="greetings-card" sx={{ width: 180, height: 180, boxShadow: "rgba(0, 0, 0, 0.35) 5px 5px 5px" }} className="category-card">
+                                            <CardContent>
+                                                <Typography sx={{ fontWeight: 600 }} className="card-progress">0/3</Typography>
+                                                <FontAwesomeIcon icon={faHandshake} className="card-icon"/>
+                                                <Typography className="card-title">SALUDOS</Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
